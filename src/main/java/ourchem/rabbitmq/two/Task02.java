b17f747437c8640c027ef4e775a8a35c93b99aab
jianghuan <2959543063@qq.com>
2021-10-08 19:22:38 +0800

    视频进度28

---
+++ src/main/java/ourchem/rabbitmq/two/Task02.java
@@ -0,0 +1,23 @@
+package ourchem.rabbitmq.two;
+
+import com.rabbitmq.client.Channel;
+import ourchem.rabbitmq.utils.RabbitMqUtils;
+
+import java.util.Scanner;
+
+public class Task02 {
+    private static final String TASK_QUEUE_NAME = "ack_queue";
+    public static void main(String[] argv) throws Exception {
+        try (Channel channel = RabbitMqUtils.getChannel()) {
+            channel.queueDeclare(TASK_QUEUE_NAME, false, false, false, null);
+            Scanner sc = new Scanner(System.in);
+            System.out.println("请输入信息");
+            while (sc.hasNext()) {
+                String message = sc.nextLine();
+                channel.basicPublish("", TASK_QUEUE_NAME, null, message.getBytes("UTF-8"));
+                System.out.println("生产者发出消息" + message);
+            }
+        }
+    }
+
+}
